25dc6616a5e4dbf1ec66578cd4215d9a3817352d
Greg Thomas <gthoma17@emich.edu>
2014-06-18 14:04:19 -0400

    it is made more different than was it before
    kthxbai

--- gentbl.py
+++ gentbl.py
@@ -7,6 +7,50 @@
 ##    ic = inline conditional, first parm is left text, second two are conditional for right field
 ##seperate multiple parameters with commas
 
+def link( fname ):
+    outString = ""
+    outString += " DC C'<a class=\"group-dataTabs\"'\n"
+    outString += " DC C' id=\"{{:" + fname + "_ID}}\"'\n"
+    outString += " DC C' href=\"#-\">'\n"
+    outString += " DC C'{{:" + fname + "_DATA}}'\n"
+    outString += " DC C'</a>'\n"
+    return outString
+def heading ( fname ):
+    return " DC C'<div class=\"group-heading\">{{:" + fname +"_DATA}}</div>'\n"
+def threeCol ( fname1, fname2 ):
+    outString = ""
+    outString += " DC C'<span class=\"group-data\" id=\"{{:" + fname1 + "_ID}}\">'\n"
+    outString += " DC C'{{:" + fname1 + "_DATA}}'\n"
+    outString += " DC C' {{:" + fname2 + "_data}}'\n"
+    outString += " DC C'</span>'\n"
+    outString += " DC C'<span class=\"group-data\" id=\"{{:" + fname1 + "_ID}}\">'\n"
+    outString += " DC C'{{:" + fname2 + "_data}}'\n"
+    outString += " DC C'</span>'\n"
+    return outString
+def condLink ( fname ):
+    outString = ""
+    outString += " DC C'{{if " + fname +"_ALINK}}'\n"
+    outString += link(fname)
+    outString += " DC C'{{else}}'\n"
+    outString += genCase(fname)
+    outString += " DC C'{{/if}}'\n" 
+    return outString
+def inCond ( data1, data2 ):
+    outString = ""
+    outString += " DC C'{{if " + data1 + "_DATA}}'\n"
+    outString += " DC C'<span class=\"group-data\" id=\"{{:" + data1 + "_ID}}\">'\n"
+    outString += " DC C'{{:" + data1 + "_DATA}}</span>'\n"
+    outString += " DC C'{{else}}'\n"
+    outString += " DC C'<span class=\"group-data\" id=\"{{:" + data2 + "_ID}}\">'\n"
+    outString += " DC C'{{:" + data2 + "_DATA}}</span>'\n"
+    outString += " DC C'{{/if}}'\n"
+    return outString
+def genCase ( fname ):
+    outString = ""
+    outString += " DC C'<span class=\"group-data\" id=\"{{:" + fname + "_ID}}\">'\n"
+    outString += " DC C'{{:" + fname + "_DATA}}</span>'\n"
+    return outString
+
 def process( line ):
     outString = ""
     line = line.rstrip()
@@ -15,43 +59,21 @@ def process( line ):
     if 'c' in parms:
         outString += " DC C'{{if " + fname +"_DATA}}'\n"
     if 'h' in parms:
-	outString += " DC C'<div class=\"group-heading\">{{:" + fname +"_DATA}}</div>'\n"
-     
+        outString += heading(fname)     
     else:
         outString += " DC C'<div class=\"group-row\">'\n"
         outString += " DC C'<span class=\"group-desc\">{{:" + fname + "_FDESC}}</span>'\n"
         if 'l' in parms:
-            outString += " DC C'<a class=\"group-dataTabs\"'\n"
-            outString += " DC C' id=\"{{:" + fname + "_ID}}\"'\n"
-            outString += " DC C' href=\"#-\">'\n"
-            outString += " DC C'{{:" + fname + "_DATA}}'\n"
-            outString += " DC C'</a>'\n"
-            outString += " DC C'</div>'\n"
+            outString += link(fname)
         elif 'cl' in parms:
-            outString += " DC C'{{if " + fname +"_ALINK}}'\n"
-            process(fname + ",l')
-            outString += " DC C'{{else}}'\n"
-            process(fname)
-            outString += " DC C'{{/if}}'\n" 
+            outString += condLink (fname)
         elif 'ic' in parms:
-            outString += " DC C'{{if " + parms[1] + "_DATA}}'\n"
-            outString += " DC C'<span class=\"group-data\" id=\"{{:" + parms[1] + "_ID}}\">'\n"
-            outString += " DC C'{{:" + parms[1] + "_DATA}}</span>'\n"
-            outString += " DC C'{{else}}'\n"
-            outString += " DC C'<span class=\"group-data\" id=\"{{:" + parms[2] + "_ID}}\">'\n"
-            outString += " DC C'{{:" + parms[2] + "_DATA}}</span>'\n"
-            outString += " DC C'{{/if}}'\n"
-            outString += " DC C'</div>'\n"
+            outString += inCond(parms[1], parms[2])
         elif '3c' in parms:
-            outString += " DC C'<span class=\"group-data\" id=\"{{:" + fname + "_ID}}\">'\n"
-            outString += " DC C'{{:" + fname + "_DATA}}'\n"
-            outString += " DC C' {{:" + parms[1] + "_data}}'\n"
-            outString += " DC C'</span>'\n"
-            outString += " DC C'</div>'\n"
+            outString += inCond(fname, parms[1])
         else:
-            outString += " DC C'<span class=\"group-data\" id=\"{{:" + fname + "_ID}}\">'\n"
-            outString += " DC C'{{:" + fname + "_DATA}}</span>'\n"
-            outString += " DC C'</div>'\n"
+            outString += genCase(fname)
+        outString += " DC C'</div">'\n"
     if 'c' in parms:
         outString += " DC C'{{/if}}'\n"
     outString += "*\n"
@@ -78,7 +100,7 @@ for line in fileinput.input():
         f.write(" DC C'</div>'\n")
         f.write(" DC C'<div class=\"group\">'\n")
     elif "###EH" in line:
-        f.write(" DC C'<div class=\"empty-heading\">{{:" + fname +"_DATA}}</div>'\n")
+        f.write(" DC C'<div class=\"group-heading-empty\">{{:" + fname +"_DATA}}</div>'\n")
         f.write("*\n"
     else:
         thisLine = process(line)
