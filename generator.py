4adb805bb2e5aea69303294ea9ff69b94608b60b
Greg Thomas <gthoma17@emich.edu>
2014-06-25 10:52:55 -0400

    fixed bug in generator that places heading row under heading
    also messed with KAZHDTMP

--- generator.py
+++ generator.py
@@ -227,15 +227,17 @@ def process( line,cols ):
     fname = parms[0]
     if 'c' in parms:
         outList.append(" DC C'{{if " + fname +"_DATA}}'\n")
+
     if 'h' in parms:
         outList += heading(fname)
-    if 'm' in parms:
+    elif 'm' in parms:
         outList += menuopt(fname)
     else:
         outList.append(" DC C'<div class=\"group-row\">'\n")
         outList += leftCol(fname)
         outList += rightCol(parms, cols)
         outList.append(" DC C'</div>'\n")
+        
     if 'c' in parms:
         outList.append(" DC C'{{/if}}'\n")
     outList.append("*\n")
